034818955ea9558e2ed4a874833773d8711f39a1
Shanker P <shanker.perumalla@gmail.com>
2020-06-09 18:03:58 +0530

    Updates

---
+++ precache-manifest.1aa570ec52b0d85229e08b400739a69c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc754b4ead94889c8021ae188b4a654c",
+    "url": "/users-list/index.html"
+  },
+  {
+    "revision": "0eab0111158d7f877e50",
+    "url": "/users-list/static/css/main.ec97d8f4.chunk.css"
+  },
+  {
+    "revision": "cdf2efb1d81865ae3af2",
+    "url": "/users-list/static/js/2.9c269de4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/users-list/static/js/2.9c269de4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eab0111158d7f877e50",
+    "url": "/users-list/static/js/main.268b90b5.chunk.js"
+  },
+  {
+    "revision": "cd7a5b4a755b99f95543",
+    "url": "/users-list/static/js/runtime-main.82d6a015.js"
+  }
+]);
